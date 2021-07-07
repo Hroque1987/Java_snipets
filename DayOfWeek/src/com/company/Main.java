57d16159fd7eaf4b218dff30ccee6e1a91fca82d
Helder Marques Roque <82022902+Hroque1987@users.noreply.github.com>
2021-07-07 23:32:58 +0100

    Switch Statement
    Some exercices using switch statement .

---
+++ DayOfWeek/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	printDayOfWeek(-4);
+    }
+
+    public static void printDayOfWeek ( int day){
+
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
+    }
+
+
+}
